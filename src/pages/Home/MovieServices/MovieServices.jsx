8d5505a93f36800e5d4e4d65e5fc5b491a0801aa
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2024-03-29 20:03:19 +0000

    Added Services Boxes

--- src/pages/Home/MovieServices/MovieServices.jsx
+++ src/pages/Home/MovieServices/MovieServices.jsx
@@ -8,6 +8,20 @@ const MovieServices = () => {
         <div className='whatmovies__services-title'>
           <h1>WhatMovies Services</h1>
         </div>
+
+        <div classname='whatmovies__services-cards'>
+          <div className='whatmovies__services-card1'>
+            <h3>We Pick movies for you</h3>
+          </div>
+
+          <div className='whatmovies__services-card2'>
+            <h3>Access to 5million+ movies</h3>
+          </div>
+
+          <div className='whatmovies__services-card3'>
+            <h3>See what everyone is watching</h3>
+          </div>
+        </div>
     </div>
   )
 }
